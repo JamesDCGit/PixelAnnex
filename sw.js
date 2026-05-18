fbf49026e7318e608a5cf81379c7b5f0e626e0b2
James De Colling <james.decolling@gmail.com>
2026-05-18 19:52:10 +1000

    Nuke fix 7

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * Version bumped per release so old caches get cleared automatically.
  */
 
-const CACHE_VERSION = 'pixelannex-v2026-05-18-nuke-clean-v20';
+const CACHE_VERSION = 'pixelannex-v2026-05-18-nuke-ownstroke-clear-v21';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
